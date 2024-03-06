f82205da3bd7174635f68dc93d0262164335df16
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-06 13:49:17 -0500

    Update build script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,19 @@ pluginManagement {
     }
 }
 
-rootProject.name = "PluginTemplate"
+dependencyResolutionManagement {
+    repositories {
+        maven("https://repo.papermc.io/repository/maven-public/")
+
+        gradlePluginPortal()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            library("worldguard", "com.sk89q.worldguard", "worldguard-bukkit").version("7.1.0-SNAPSHOT")
+        }
+    }
+}
+
+rootProject.name = "SimpleFlags"
