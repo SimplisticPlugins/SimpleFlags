234631d8fc0ecfb89e17322d494f8b73fcd057a1
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-06 20:16:34 -0500

    Add command api

---
+++ src/main/java/com/ryderbelserion/simpleflags/platform/BaseCommand.java
@@ -0,0 +1,56 @@
+package com.ryderbelserion.simpleflags.platform;
+
+import ch.jalu.configme.SettingsManager;
+import com.ryderbelserion.simpleflags.SimpleFlags;
+import com.ryderbelserion.simpleflags.platform.impl.Config;
+import com.ryderbelserion.simpleflags.platform.impl.Locale;
+import com.ryderbelserion.simpleflags.platform.impl.Metrics;
+import dev.triumphteam.cmd.bukkit.annotation.Permission;
+import dev.triumphteam.cmd.core.annotations.Command;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+@Command("simpleflags")
+public class BaseCommand {
+
+    @NotNull
+    private final SimpleFlags plugin = JavaPlugin.getPlugin(SimpleFlags.class);
+
+    @NotNull
+    private final Metrics metrics = this.plugin.getMetrics();
+
+    @NotNull
+    private final SettingsManager config = this.plugin.getOptions();
+
+    @NotNull
+    private final SettingsManager locale = this.plugin.getLocale();
+
+    @NotNull
+    private final MiniMessage message = MiniMessage.miniMessage();
+
+    @Command
+    @Permission("simpleflags.help")
+    public void help(CommandSender sender) {
+        this.locale.getProperty(Locale.help).forEach(line -> sender.sendMessage(this.message.deserialize(line)));
+    }
+
+    @Command("reload")
+    @Permission("simpleflags.reload")
+    public void reload(CommandSender sender) {
+        // Reload the config files.
+        this.config.reload();
+        this.locale.reload();
+
+        // Check if metrics needs to be enabled or not.
+        if (!this.config.getProperty(Config.toggle_metrics)) {
+            this.metrics.stop();
+        } else {
+            this.metrics.start();
+        }
+
+        // Send the reload message.
+        sender.sendMessage(this.message.deserialize(this.locale.getProperty(Locale.reload_plugin).replaceAll("\\{prefix}", this.config.getProperty(Config.command_prefix))));
+    }
+}
